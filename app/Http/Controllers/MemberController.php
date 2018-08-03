414ed18d05a263c61b8ee7a538f14d86e55870a6
ankunde <1175386563@qq.com>
2018-08-03 10:35:47 +0800

    day13项目上线

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -15,6 +15,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Validator;
 
@@ -93,6 +94,7 @@ class MemberController extends Controller
     public function addorder(Request $request)
     {
         //>>1.开启事物添加数据
+
         $order_id =  DB::transaction(function () use($request){
             //>>1.1传过来的地址id
             $id = $request->address_id;
@@ -140,8 +142,61 @@ class MemberController extends Controller
             }
                 //1.8再去修改总价
                 $orders->update(['total'=>$total]);
+            $tel =Auth::user()->tel;
+            $params = [];
+            $shops= Shops::where('id',$shop_id)->value('shop_name');
+
+            // *** 需用户填写部分 ***
+
+            // fixme 必填: 请参阅 https://ak-console.aliyun.com/ 取得您的AK信息
+            $accessKeyId = "LTAImjBFWmSegGAH";
+            $accessKeySecret = "cXNj0wekciFyEfUPM5u84zsciOJonE";
+
+            // fixme 必填: 短信接收号码
+            $params["PhoneNumbers"] = $tel;//request()->input('tel',17683287216);//
+            // fixme 必填: 短信签名，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
+            $params["SignName"] = "安坤";
+
+            // fixme 必填: 短信模板Code，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
+            $params["TemplateCode"] = "SMS_141350001";
+
+            // fixme 可选: 设置模板参数, 假如模板中存在变量需要替换则为必填项
+            $params['TemplateParam'] = Array(
+                "name" =>$shops,
+//            "product" => "阿里通信"
+            );
+
+
+
+            // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+            if (!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+                $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+            }
+// 初始化SignatureHelper实例用于设置参数，签名以及发送请求
+            $helper = new SignatureHelper();
+            // 此处可能会抛出异常，注意catch
+            $content = $helper->request(
+                $accessKeyId,
+                $accessKeySecret,
+                "dysmsapi.aliyuncs.com",
+                array_merge($params, array(
+                    "RegionId" => "cn-hangzhou",
+                    "Action" => "SendSms",
+                    "Version" => "2017-05-25",
+                ))
+            // fixme 选填: 启用https
+            // ,true
+            );
+            Mail::raw('商户认证审核已经通过',function ($message){
+                $message->subject('饿了吧外卖');
+                $message->to('yukuaiguyi@163.com');
+            });
             return $orders->id;
         });
+
+
+
+
         //>>2.返回需要的数据
         return [
         "status"=> "true",
